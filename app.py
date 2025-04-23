45f25c12b036527e331fc075fc0288b2b371ee71
Danny8174 <insidesecurity@me.com>
2025-04-24 07:27:56 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from synthax_cli import run_synthax_code
 
-app = Flask(__name__)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
+
 
 @app.route("/generate", methods=["POST"])
 def generate():
